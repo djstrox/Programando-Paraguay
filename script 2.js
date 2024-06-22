6afec2107f49ccb6be7a2f607613bf5108b954db
Hiam <50251398+djstrox@users.noreply.github.com>
2024-06-22 14:03:01 -0400

    Create script 2.js

---
+++ script 2.js
@@ -0,0 +1,23 @@
+num1 = 8;
+num2 = 5;
+num3 = 7;
+console.log("El pequenho john quiere saber cual de sus 3 numeros es el maypr y si los dos primeros son iguales")
+function comparar{
+    if (num1 === num2) {
+        console.log("los primeros dos numeros son iguales");
+        console.log("Los dos primeros numeros no son iguales u freaky ahh")
+    }
+    else if (num1 > num2, num1 > num3,) {
+        console.log(num1, "es el mayor de todos")
+        console.log("Los dos primeros numeros no son iguales u freaky ahh")
+    }
+    else if (num2 > num1, num2 > num3,) {
+        console.log(num2, "es el mayor de todos")
+        console.log("Los dos primeros numeros no son iguales u freaky ahh")
+    }
+    else{
+        console.log(num3, "es el mayor de todos")
+        console.log("Los dos primeros numeros no son iguales u freaky ahh")
+    }
+}
+comparar();

a89c56aab58d4e126faaf8986aba8904aaf216bd
Hiam <50251398+djstrox@users.noreply.github.com>
2024-06-22 14:16:32 -0400

    script 2.js

--- script 2.js
+++ script 2.js
@@ -1,23 +1,22 @@
-num1 = 8;
-num2 = 5;
-num3 = 7;
 console.log("El pequenho john quiere saber cual de sus 3 numeros es el maypr y si los dos primeros son iguales")
-function comparar{
+function comparar(num1, num2, num3) {
     if (num1 === num2) {
         console.log("los primeros dos numeros son iguales");
-        console.log("Los dos primeros numeros no son iguales u freaky ahh")
+        console.log("Los dos primeros numeros no son iguales u freaky ahh");
     }
-    else if (num1 > num2, num1 > num3,) {
-        console.log(num1, "es el mayor de todos")
-        console.log("Los dos primeros numeros no son iguales u freaky ahh")
+    else if (num1 > num2, num1 > num3) {
+        console.log(num1, "es el mayor de todos");
+        console.log("Los dos primeros numeros no son iguales u freaky ahh");
     }
-    else if (num2 > num1, num2 > num3,) {
-        console.log(num2, "es el mayor de todos")
-        console.log("Los dos primeros numeros no son iguales u freaky ahh")
+    else if (num2 > num1, num2 > num3) {
+        console.log(num2, "es el mayor de todos");
+        console.log("Los dos primeros numeros no son iguales u freaky ahh");
     }
-    else{
+    else {
         console.log(num3, "es el mayor de todos")
-        console.log("Los dos primeros numeros no son iguales u freaky ahh")
+        console.log("Los dos primeros numeros no son iguales u freaky ahh");
     }
 }
-comparar();
+comparar(4, 52, 11);
+comparar(6, 8, 13);
+comparar(5, 98, 1126);
